644669ac397ffeac3f522beb83fd1f059dcb5634
eunjeong <cs801458j@gmail.com>
2018-05-05 20:46:45 +0900

    file upload example

---
+++ file_upload_test/file_upload.js
@@ -0,0 +1,5 @@
+
+//var selectFile = document.getElementById('upload_file').files[0];
+var inputElement = document.getElementById('upload_file');
+inputElement.addEventListener("change", handleFiles, false);
+
